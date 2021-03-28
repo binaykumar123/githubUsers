4cdf4044727132f5ea6ad60f80926431683948ff
Binay <kumarbinay999@gmail.com>
2021-03-28 18:44:59 +0530

    first commit

---
+++ app/src/main/java/com/binay/android/githubusers/view/UsersActivity.kt
@@ -0,0 +1,12 @@
+package com.binay.android.githubusers.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.binay.android.githubusers.R
+
+class UsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
